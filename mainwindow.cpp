92e63a86dea23dd65284bbf78e1e84bc5eead017
bright.jiang <bright.jiang@amd.com>
2021-04-14 17:01:09 +0800

    save

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include <QFileDialog>
+#include <QDebug>
+#include <QString>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +15,16 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButtonOpenImgFile_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+            tr("Open Address Book"), "",
+            tr("img file (*.img *.bin);;All Files (*)"));
+    qDebug() << "img file name -> " <<  fileName << endl;
+    if(fileName.size() != 0)
+    {
+
+    }
+
+}
