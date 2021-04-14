3b750bc0c6a02ab41708235a4e174c6abb9a915a
bright.jiang <bright.jiang@amd.com>
2021-04-14 17:29:43 +0800

    save

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,8 @@
 #include <QFileDialog>
 #include <QDebug>
 #include <QString>
+#include <QMessageBox>
+#include <blkid.h>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -19,12 +21,26 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButtonOpenImgFile_clicked()
 {
     QString fileName = QFileDialog::getOpenFileName(this,
-            tr("Open Address Book"), "",
+            tr("Open Address Book"), "/nfs/share/disk/",
             tr("img file (*.img *.bin);;All Files (*)"));
     qDebug() << "img file name -> " <<  fileName << endl;
-    if(fileName.size() != 0)
+    if(fileName.size() == 0)
     {
-
+        QMessageBox::critical(this,"open img fail","open img fail");
+        return;
     }
+
+
+    blkid_cache cache;
+    std::string str = fileName.toStdString();
+    int ret = blkid_get_cache(&cache, str.c_str());
+    qDebug() << "Open file: " << fileName << " ret: " << ret;
+    if(ret < 0 )
+    {
+        QMessageBox::critical(this,"open img fail","open img fail");
+        return;
+    }
+
+
 
 }
